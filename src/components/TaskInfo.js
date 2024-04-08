8141c3f170517261452c3035cd43db13efd2b24e
Vincent Tampos <56838797+glitchcodes@users.noreply.github.com>
2024-04-08 08:21:13 +0000

    Fix NaN on Percent Done

--- src/components/TaskInfo.js
+++ src/components/TaskInfo.js
@@ -1,21 +1,21 @@
-function TaskInfo({tasks}) {
-  const completedItems = tasks.filter((task) => task.isChecked).length
+function TaskInfo({ tasks }) {
+  const completedItems = tasks.filter((task) => task.isChecked).length;
 
-  const percentDone = Math.round((completedItems / tasks.length) * 100);
+  const percentDone = Math.round((completedItems / tasks.length) * 100) || 0;
 
   return (
     <div className="w-1/2 bg-slate-300 px-3 py-5 rounded-md">
       <p className="text-xl font-bold text-gray-900 mb-4">
-        Total Items: <span>{ tasks.length }</span>
+        Total Items: <span>{tasks.length}</span>
       </p>
       <p className="text-xl font-bold text-gray-900 mb-4">
-        Completed Items: <span>{ completedItems }</span>
+        Completed Items: <span>{completedItems}</span>
       </p>
       <p className="text-xl font-bold text-gray-900">
         Percent Done: <span>{percentDone}%</span>
       </p>
     </div>
-  )
+  );
 }
 
-export default TaskInfo
+export default TaskInfo;
